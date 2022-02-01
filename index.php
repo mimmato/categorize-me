7d382936fae3f237fab74128f3e7500e5f0fe706
Mihail Mihaylov <mimmato@gmail.com>
2022-02-01 22:07:08 +0200

    categorize me project

---
+++ index.php
@@ -0,0 +1,39 @@
+    <!DOCTYPE html>
+    <html>
+    	<head>
+
+    		<meta charset="utf-8">
+    		<title>Categorize Me!</title>
+    		<link href="style.css" rel="stylesheet" type="text/css">
+
+    	</head>
+
+    	<body>
+        <nav class="navtop">
+        	<div>
+        		<h1>Cetegorize me (ticketing)</h1>
+                <a href="submit-ticket.php">Submit ticket</a>
+        	</div>
+        </nav>
+
+
+
+
+<form>
+
+            <label for="title">Email issues</label></br>
+            <input type="text" name="title" placeholder="This box will include information that the client puts in the submit-ticket.php form" id="title" required size="60" maxLength="60">
+        	<br>
+
+            <label for="email">WordPress</label></br>
+            <input type="email" name="email" placeholder="This box will include information that the client puts in the submit-ticket.php form" id="email" required size="60" maxLength="60"></br>
+
+            <label for="msg">Server error</label></br>
+            <input type="text" id="name" name="name" required required size="60" maxLength="60" placeholder="This box will include information that the client puts in the submit-ticket.php form"><br>
+
+</form>
+
+
+
+</body>
+</html>
---
+++ submit-ticket.php
@@ -0,0 +1,38 @@
+<html>
+<body>
+	<head>
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+
+
+	<nav class="navtop">
+		<div>
+			<h1>Cetegorize me (ticketing)</h1>
+			<a href="index.php">Review tickets</a>
+		</div>
+	</nav>
+
+<form>
+
+        <label for="title">Name</label></br>
+        <input type="text" name="title" placeholder="Please enter your first and last name" id="title" required size="60" maxLength="60">
+		<br>
+
+        <label for="email">Email</label></br>
+        <input type="email" name="email" placeholder="youremail@yourdomain.com" id="email" required size="60" maxLength="60"></br>
+
+        <label for="msg" autofocus="">Message</label></br>
+        <textarea name="msg" placeholder="Specify your request..." id="msg" required rows="5" cols="60"></textarea></br>
+
+        <input type="submit" value="Create">
+
+</form>
+
+    <?php if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php endif; ?>
+
+
+
+</body>
+</html>
