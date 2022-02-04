fba730dc5996c298ee74e675769b0a6d0468054c
Mihail Mihaylov <mimmato@gmail.com>
2022-02-05 00:21:26 +0200

    Testing the usage of multidimensional arrays where data is generated from an external .csv file.

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 $csvfile=file("data.csv");
 $data=[];
-foreach ($csvfile as $line){
+foreach ($csvfile as $line => $keys) {
     $data[] = str_getcsv($line);
-
 }
 //print_r($data);
+
+    echo $keys . " ";
+    //print_r($keys);
 ?>
 
 <!DOCTYPE html>
@@ -28,11 +30,11 @@ foreach ($csvfile as $line){
 
 
 
-
+a.cs
 <div class="input-field-div">
     <p align="center"> Email issues </p>
 
-    <textarea readonly required type="text" id="email" name="email" rows="10" placeholder="This box will include information that the client puts in the submit-ticket.php form that relates to Email issues">
+    <textarea readonly required type="text" id="email" name="email" rows="40" placeholder="This box will include information that the client puts in the submit-ticket.php form that relates to Email issues">
 
 <?php if($_POST["requestType"]=="Email") {
     echo "Name: " . $_POST ["name"], PHP_EOL;
@@ -40,6 +42,58 @@ foreach ($csvfile as $line){
     echo "Issue description: " . $_POST["msg"], PHP_EOL;
     }
 ?>
+
+
+
+
+<?php
+$csvfile=file("data.csv");
+$data=[];
+foreach ($csvfile as $line) {
+        echo $line . " ";
+        echo "\n";
+    $data[] = str_getcsv($line);
+// foreach ($data as $value){
+//     echo $value . "";
+// }
+
+// $mail="Email";
+// if ($data[0][2] == Email){
+//  echo $mail;
+// }
+// print_r($mail);
+
+}
+
+
+ print_r($data);
+// echo $data[0][0];
+//
+// if($data=$data[0][0])
+// echo $data[0][2];
+
+
+// if($data == $data[2]) {
+//    echo "Name: " . $data[0], PHP_EOL;
+//     echo "Name: " . $data[1], PHP_EOL;
+//    echo "Name: " . $data[3], PHP_EOL;
+// }
+
+// $found = in_array(Email,$data2);
+//
+// if($found){
+//     echo $data2;
+// }else
+//     echo "NO";
+
+
+
+
+//print_r($data);
+?>
+
+
+
     </textarea></br>
 </div>
 
@@ -47,20 +101,19 @@ foreach ($csvfile as $line){
 
 <div class="input-field-div">
     <p align="center"> WordPress </p>
-    <textarea readonly type="text" id="name" name="name" rows="10" placeholder="This box will include information that the client puts in the submit-ticket.php form that relates to WordPress">
+    <textarea readonly type="text" id="name" name="name" rows="40" placeholder="This box will include information that the client puts in the submit-ticket.php form that relates to WordPress">
 <?php if($_POST["requestType"]=="WordPress") {
     echo "Name: " . $_POST ["name"], PHP_EOL;
     echo "Email address: " . $_POST["email"], PHP_EOL;
     echo "Issue description: " . $_POST["msg"], PHP_EOL;
     }
 ?>
-
     </textarea></br>
 </div>
 
 <div class="input-field-div">
     <p align="center"> Server error </p>
-    <textarea readonly type="text" name="errors" id="errors" rows="10" placeholder="This box will include information that the client puts in the submit-ticket.php form that relates to Server errors">
+    <textarea readonly type="text" name="errors" id="errors" rows="40" placeholder="This box will include information that the client puts in the submit-ticket.php form that relates to Server errors">
 
 <?php if($_POST["requestType"]=="ServerError") {
     echo "Name: " . $_POST ["name"], PHP_EOL;
